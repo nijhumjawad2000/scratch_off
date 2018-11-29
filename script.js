484cefc532adae65653d7676ba272b9c40390141
nijhum_jawad2000 <njawad@techlaunch.io>
2018-11-28 22:05:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function mySound(){
+    var mySound = document.getElementById("mySound")
+    mySound.play();
+}
+var myModal = document.getElementById("myModal");
+
+function pic1(){
+    var pic1 = document.getElementById("pic1");
+    pic1.src="scratching.gif";
+    setTimeout(() => pic1.style.opacity="0", 4000);
+    setTimeout(() => mySound(), 4000);
+    myModal.style.visibility="hidden";
+    setTimeout(() =>  myModal.style.visibility="visible", 4000);
+    //mySound()
+}
+function pic2(){
+    var pic2 = document.getElementById("pic2");
+    pic2.src="scratching.gif";
+    setTimeout(() => pic2.style.opacity="0", 4000);
+    setTimeout(() => mySound(), 4000);
+    myModal.style.visibility="hidden";
+    setTimeout(() =>  myModal.style.visibility="visible", 4000);
+}
+function pic3(){
+    var pic3 = document.getElementById("pic3");
+    pic3.src="scratching.gif";
+    setTimeout(() => pic3.style.opacity="0", 4000);
+    setTimeout(() => mySound(), 4000);
+    myModal.style.visibility="hidden";
+    setTimeout(() =>  myModal.style.visibility="visible", 4000);
+}
+function pic4(){
+    var pic4 = document.getElementById("pic4");
+    pic4.src="scratching.gif";
+    setTimeout(() => pic4.style.opacity="0", 4000);
+    setTimeout(() => mySound(), 4000);
+    myModal.style.visibility="hidden";
+    setTimeout(() =>  myModal.style.visibility="visible", 4000);
+}
+function pic5(){
+    var pic5 = document.getElementById("pic5");
+    pic5.src="scratching.gif";
+    setTimeout(() => pic5.style.opacity="0", 4000);
+    setTimeout(() => mySound(), 4000);
+    myModal.style.visibility="hidden";
+    setTimeout(() =>  myModal.style.visibility="visible", 4000);
+}
+function pic6(){
+    var pic6 = document.getElementById("pic6");
+    pic6.src="scratching.gif";
+    setTimeout(() => pic6.style.opacity="0", 4000);
+    setTimeout(() => mySound(), 4000);
+    myModal.style.visibility="hidden";
+    setTimeout(() =>  myModal.style.visibility="visible", 4000);
+}
+
